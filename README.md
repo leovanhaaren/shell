220bca2d945a4450e11009be417d68da878562d0
Leo van Haaren <leovhaaren@gmail.com>
2021-11-23 15:46:31 +0100

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# shell
+Aliases and other shell stuff
+
+## Installation
+
+1. Clone the repo to `~/code/shell`
+2. Add the following to `~/.bashrc`
+
+```sh
+source ~/code/shell/setup.sh
+```
+
+## Add custom local config
+Create a `custom.sh` file in this directory, this will be sourced automatically.
---
+++ setup.sh
@@ -0,0 +1,19 @@
+alias g='git'
+alias gs='git status'
+alias ga='git add -A'
+alias gc='git commit'
+alias gpl='git pull'
+alias gpsh='git push -u origin HEAD'
+alias gd='git diff'
+alias gp='git push'
+alias gnb='git checkout -b'
+alias gco='git checkout'
+alias gb='git branch'
+alias rn='git log --pretty="- %s (%an)" $0'
+alias gm='git merge'
+
+alias sail='[ -f sail ] && bash sail || bash vendor/bin/sail'
+
+export KUBECONFIG=.kubeconfig:$HOME/.kube/config
+
+source ~/code/shell/custom.sh
